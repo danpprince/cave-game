659a206ad38db24e927d2b0cf9b0e4e513625244
Mama_Dutchy <datpoland@gmail.com>
2022-09-05 12:45:43 -0400

    throwing torch works hold to charge, error on game ending, is a bug in the new input system still unresolved as of 1.4.2

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    //General Input Getting
+    //General Input Getting \\
     public CharacterController cc;
     public PlayerInput playerControls;
 
-    //For Movement and Moving Acessories
-    public float MoveSpeed = 5f;
+    // For Movement and Moving Acessories \\
     private InputAction MoveController;
+    public float MoveSpeed = 5f;
     private Vector2 MoveDirection = Vector2.zero;
     private Vector3 Velocity;
     public float Gravity = -9.8f;
@@ -30,49 +30,53 @@ public class Movement : MonoBehaviour
     public Transform playerBody;
     private float X_Rotation = 0;
 
-    //For Torch Tossing\\
-    public GameObject Torch;
+    // For Torch Tossing \\
     private InputAction TossInput;
+    public GameObject Torch;
+    public GameObject TorchSpawnPoint;
+    public float UpwardsForce = 5.5f, ForwardsForce = 8f;
+    private float DefaultForwardsForce;
+    public float TorchForceMultiplier = 2f;
+   
 
 
     void Start()
     {
-        MoveController = playerControls.actions["Move"];
-        Look = playerControls.actions["Look"];
-        TossInput = playerControls.actions["Toss"];
-
         Cursor.lockState = CursorLockMode.Locked;
+        DefaultForwardsForce = ForwardsForce;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement setup
         MoveDirection = MoveController.ReadValue<Vector2>();
         MousePosition = Look.ReadValue<Vector2>();
         isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
 
         // Looking stuff \\
-        float Look_X = MousePosition.x * X_AimSensitivity;
-        float Look_Y = MousePosition.y * Y_AimSensitivity;
+        float Look_X = MousePosition.x * X_AimSensitivity *Time.deltaTime * 100;
+        float Look_Y = MousePosition.y * Y_AimSensitivity *Time.deltaTime * 100;
         X_Rotation -= Look_Y;
         X_Rotation = Mathf.Clamp(X_Rotation, -90f, 90f);
 
-        //Rotates entire Player object left and right \\
+        // Rotates entire Player object left and right \\
         playerBody.Rotate(Vector3.up * Look_X);
 
-        //Allow the camera to look up and down\\
+        // Allow the camera to look up and down \\
         camera.transform.localRotation = Quaternion.Euler(X_Rotation, 0f, 0f);
 
-
+        //Toss a torch
+      
     }
 
     private void FixedUpdate()
     {
-        //Moves the Player around
+        // Moves the Player around \\
         Vector3 Move = transform.right * MoveDirection.x + transform.forward * MoveDirection.y;
         cc.Move(Move * MoveSpeed);
 
-        //Gives the Player Gravity
+        // Gives the Player Gravity \\
         Velocity.y += Gravity;
         cc.Move(Velocity * Time.deltaTime);
 
@@ -80,10 +84,53 @@ public class Movement : MonoBehaviour
         {
             Velocity.y = -2f;
         }
-
-     
     }
 
-  
+    private void ChargingTorch(InputAction.CallbackContext obj)
+    {
+        print("Holding");
+    }
 
+    private void TossTorch(InputAction.CallbackContext obj)
+    {
+            print(obj.duration);
+            ForwardsForce += ForwardsForce * (float)obj.duration * TorchForceMultiplier;
+            GameObject TorchInstance = Instantiate(Torch, TorchSpawnPoint.transform.position, camera.transform.rotation);
+            Rigidbody torch_rb = TorchInstance.GetComponent<Rigidbody>();
+            torch_rb.velocity = this.Velocity;
+            Vector3 ForceToAdd = camera.transform.forward * ForwardsForce + TorchSpawnPoint.transform.up * UpwardsForce;
+            torch_rb.AddForce(ForceToAdd, ForceMode.Impulse);
+            torch_rb.AddTorque(transform.up * Random.Range(0, 20));
+            print("Rock and Stone brother: " + ForwardsForce);
+            ForwardsForce = DefaultForwardsForce;
+            
+    }
+
+
+    private void OnEnable()
+    { 
+        playerControls.ActivateInput();
+
+        MoveController = playerControls.actions["Move"];
+        Look = playerControls.actions["Look"];
+        TossInput = playerControls.actions["Toss"];
+
+        MoveController.Enable();   
+        Look.Enable();
+
+        TossInput.started += ChargingTorch;
+        TossInput.canceled += TossTorch;
+        TossInput.Enable();
+        
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnDisable()
+    {
+        playerControls.DeactivateInput();
+        MoveController.Disable();
+        Look.Disable();
+        TossInput.Disable();
+    }
 }
