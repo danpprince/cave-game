dbc547f4f33847f164b8ba2ab512762426bc387d
Reuben Miller <reubenmiller98@gmail.com>
2022-09-08 20:19:51 -0400

    Made jumping kinda work

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,29 +9,42 @@ public class Movement : MonoBehaviour
     public PlayerInput playerControls;
     private InputAction MoveController;
     private Vector2 moveDirection = Vector2.zero;
+    private Vector3 velocity;
+    public float Speed = 2f;
+    public float jump = 10f;
+    public float Gravity = -9.8f;
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        MoveController = playerControls.actions["Move"];
-    }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = MoveController.ReadValue<Vector2>();
+            // for jump
+            if (Input.GetKeyUp(KeyCode.Space) && transform.position.y < -0.51f)
+            // (-0.5) change this value according to your character y position + 1
+            {
+                velocity.y = jump;
+            }
+            else
+            {
+                velocity.y += Gravity * Time.deltaTime;
+            }
+            cc.Move(velocity * Time.deltaTime);
+        
     }
 
     private void FixedUpdate()
     {
         Vector3 Move = transform.right * moveDirection.x + transform.forward * moveDirection.y;
         cc.Move(Move * MoveSpeed * Time.deltaTime);
+        
 
     }
 
-  
+    private void OnEnable()
+    {
+        MoveController = playerControls.actions["Move"];
+    }
+
 
 }
