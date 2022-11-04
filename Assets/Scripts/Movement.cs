2afe18ec24c470956cf122662234a17c1e97afb6
Reuben Miller <reubenmiller98@gmail.com>
2022-11-03 20:30:30 -0400

    jumping gUd

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,49 +11,9 @@ public class Movement : MonoBehaviour
     //For Movement and Moving Acessories
     public float MoveSpeed = 5f;
     private InputAction MoveController;
-
     private Vector2 moveDirection = Vector2.zero;
     private Vector3 velocity;
-    public float Speed = 2f;
-    public float jump = 10f;
-    public float Gravity = -9.8f;
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveDirection = MoveController.ReadValue<Vector2>();
-        MousePosition = Look.ReadValue<Vector2>();
-        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
-
-        // Looking stuff \\
-        float Look_X = MousePosition.x * X_AimSensitivity;
-        float Look_Y = MousePosition.y * Y_AimSensitivity;
-        X_Rotation -= Look_Y;
-        X_Rotation = Mathf.Clamp(X_Rotation, -90f, 90f);
-
-        //Rotates entire Player object left and right \\
-        playerBody.Rotate(Vector3.up * Look_X);
-
-        //Allow the camera to look up and down\\
-        camera.transform.localRotation = Quaternion.Euler(X_Rotation, 0f, 0f);
-        moveDirection = MoveController.ReadValue<Vector2>();
-
-        // for jump
-        if (Input.GetKeyUp(KeyCode.Space) && transform.position.y < + 0.51f)
-        // (-0.5) change this value according to your character y position + 1
-        {
-            velocity.y = jump;
-        }
-        else
-        {
-            velocity.y += Gravity * Time.deltaTime;
-        }
-        cc.Move(velocity * Time.deltaTime);
-    }
-        
-    private Vector2 MoveDirection = Vector2.zero;
-    private Vector3 Velocity;
+    public float Gravity = -3.8f;
     public Transform GroundCheck;
     public float GroundDistance = 0.4f;
     public LayerMask GroundMask;
@@ -70,6 +30,11 @@ public class Movement : MonoBehaviour
     public Transform playerBody;
     private float X_Rotation = 0;
 
+    // for jump
+    private InputAction JumpInput;
+    public float jump = 10f;
+
+
     //For Torch Tossing\\
     public GameObject Torch;
     private InputAction TossInput;
@@ -77,7 +42,6 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        MoveController = playerControls.actions["Move"];
         Look = playerControls.actions["Look"];
         TossInput = playerControls.actions["Toss"];
 
@@ -85,29 +49,64 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        moveDirection = MoveController.ReadValue<Vector2>();
+        MousePosition = Look.ReadValue<Vector2>();
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        // Looking stuff \\
+        float Look_X = MousePosition.x * X_AimSensitivity;
+        float Look_Y = MousePosition.y * Y_AimSensitivity;
+        X_Rotation -= Look_Y;
+        X_Rotation = Mathf.Clamp(X_Rotation, -90f, 90f);
+
+        //Rotates entire Player object left and right \\
+        playerBody.Rotate(Vector3.up * Look_X);
+
+        //Allow the camera to look up and down\\
+        camera.transform.localRotation = Quaternion.Euler(X_Rotation, 0f, 0f);
+        moveDirection = MoveController.ReadValue<Vector2>();
+
+        
+    }
 
     private void FixedUpdate()
     {
         //Moves the Player around
-        Vector3 Move = transform.right * MoveDirection.x + transform.forward * MoveDirection.y;
+        Vector3 Move = transform.right * moveDirection.x + transform.forward * moveDirection.y;
         cc.Move(Move * MoveSpeed);
-//>>>>>>> 68ca32432772de06cacc6b211a80ae56ced9a7b7
+        cc.Move(velocity * Time.deltaTime);
+        velocity.y += Gravity;
 
-        //Gives the Player Gravity
-        //Velocity.y += Gravity;
-        //cc.Move(Velocity * Time.deltaTime);
-
-        if(isGrounded && Velocity.y < 0)
+        if(isGrounded && velocity.y < 0)
         {
-            //Velocity.y = -2f;
+            velocity.y = -2f;
         }
-
-     
     }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        if (isGrounded)
+        // (-0.5) change this value according to your character y position + 1
+        {
+            velocity.y += jump;
+        }
+    }
+
 
     private void OnEnable()
     {
         MoveController = playerControls.actions["Move"];
+        JumpInput = playerControls.actions["Jump"];
+
+        JumpInput.started += Jump;
+        JumpInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        JumpInput.Disable();
     }
 
 
