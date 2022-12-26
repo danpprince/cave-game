1f85e347c76c5f8232e2f4d45af0a82df2d13b39
Dan Prince <danpprince@gmail.com>
2022-12-26 16:08:59 -0500

    Recurse into all child tunnel endpoints, set new objects as children of source

--- Assets/Scripts/TunnelPathGenerator.cs
+++ Assets/Scripts/TunnelPathGenerator.cs
@@ -26,16 +26,18 @@ public class TunnelPathGenerator : MonoBehaviour
 
     public void GeneratePaths()
     {
-        GeneratePathsRecursive(startingTunnelEndpoint.position, 0);
+        GeneratePathsRecursive(startingTunnelEndpoint, 0);
 
     }
 
-    private void GeneratePathsRecursive(Vector3 sourceTunnelPosition, int depth)
+    private void GeneratePathsRecursive(Transform sourceTunnelTransform, int depth)
     {
         if (depth >= maxRecursionDepth)
         {
             return;
         }
+
+        Vector3 sourceTunnelPosition = sourceTunnelTransform.position;
 
         // Deeper rooms always go down in Y, but can be positive or negative in X and Z
         float xSign = Random.value < 0.5f ? 1 : -1;
@@ -47,11 +49,12 @@ public class TunnelPathGenerator : MonoBehaviour
         );
         Quaternion destinationRoomRotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
         GameObject room = Instantiate(roomPrefab, destinationRoomPosition, destinationRoomRotation, transform);
+        room.transform.parent = sourceTunnelTransform;
 
         // Set the path destination to the closest TunnelEndpoint child in the new room
         Transform pathDestination = null;
         float pathDistance = float.PositiveInfinity;
-        List<Vector3> childPositionsToRecurseInto = new List<Vector3>();
+        List<Transform> childTransformsToRecurseInto = new List<Transform>();
         for (int childIndex = 0; childIndex < room.transform.childCount; childIndex++)
         {
             GameObject childObject = room.transform.GetChild(childIndex).gameObject;
@@ -59,6 +62,9 @@ public class TunnelPathGenerator : MonoBehaviour
             {
                 continue;
             }
+
+            childTransformsToRecurseInto.Add(childObject.transform);
+
             if (pathDestination is null)
             {
                 pathDestination = childObject.transform;
@@ -75,15 +81,11 @@ public class TunnelPathGenerator : MonoBehaviour
                 print($"Shorter path found, now destination {pathDestination} with distance {pathDistance}");
                 continue;
             }
-
-            // TODO: This will not work properly if the shortest distance is not found first
-            print($"Shorter path found, now destination {pathDestination} with distance {pathDistance}");
-            childPositionsToRecurseInto.Add(childObject.transform.position);
         }
 
         // Create the bezier path to the selected destination
         GameObject pathObject = new GameObject("Path");
-        pathObject.transform.parent = gameObject.transform;
+        pathObject.transform.parent = sourceTunnelTransform.transform;
         List<Vector3> pathPoints = new List<Vector3> {
             sourceTunnelPosition, pathDestination.position
         };
@@ -94,9 +96,9 @@ public class TunnelPathGenerator : MonoBehaviour
         tunnelPathCreator.bezierPath.ControlPointMode = BezierPath.ControlMode.Aligned;
         tunnelPathCreator.bezierPath.ControlPointMode = BezierPath.ControlMode.Automatic;
 
-        foreach (Vector3 childPosition in childPositionsToRecurseInto)
+        foreach (Transform childTransform in childTransformsToRecurseInto)
         {
-            GeneratePathsRecursive(childPosition, depth + 1);
+            GeneratePathsRecursive(childTransform, depth + 1);
         }
     }
 }
