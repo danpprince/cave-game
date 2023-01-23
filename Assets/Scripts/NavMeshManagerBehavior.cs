1d0deacff958b018fb81f855cc25a95a262945e0
Colin Farrell <colinfarrell@protonmail.com>
2023-01-23 00:11:17 -0500

    try to connect navmeshs together with offMeshLinks at runtime, not working rn

--- Assets/Scripts/NavMeshManagerBehavior.cs
+++ Assets/Scripts/NavMeshManagerBehavior.cs
@@ -1,11 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NavMeshManagerBehavior : MonoBehaviour
 {
-    private Mesh mesh;
+    //private Mesh mesh;
+
+    [SerializeField]
+    private bool enableDrawTriangles = false;
 
     [SerializeField]
     private GameObject agentPrefab;
@@ -14,34 +16,94 @@ public class NavMeshManagerBehavior : MonoBehaviour
     private GameObject vertexPrefab;
 
     [SerializeField]
-    private bool enableDrawTriangles = false;
+    private float linkSearchLength;
 
-    // declare list of Vector3 for navMeshEdgeCorners
     private List<Vector3> navMeshEdgeCorners = new List<Vector3>();
+
+    private List<Vector3> navMeshEdges = new List<Vector3>();
+
+    private List<GameObject> navMeshVertices = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        // Find the mesh from the "Terrain" object in the scene
-        mesh = GameObject.Find("Terrain").GetComponent<MeshFilter>().mesh;
+        // Run the NavMeshTriangulation
+        NavMesh2Triangles();
+        
+        
+        
+        // Run the LinkBuilder
+        LinkBuilder(linkSearchLength);
 
-        // Find the agentTypeID for the agentPrefab
-        int agentTypeID = agentPrefab.GetComponent<NavMeshAgent>().agentTypeID;
+        Debug.Log("Count of NavMeshVertices: " + navMeshVertices.Count);
+    }
 
-        // Find the NavMeshBuildSettings for the agentTypeID
-        NavMeshBuildSettings settings = NavMesh.GetSettingsByID(agentTypeID);
+    // Update is called once per frame
+    void Update()
+    {
 
-        // Count each triangle on the mesh
-        int triangleCount = mesh.triangles.Length / 3;
-        Debug.Log("Terrain triangles counted: " + triangleCount);
+    }
 
+
+    // Check vertices near each other (within a radius)
+    // that do not have a NavMesh hit at the midpoint of their centers   
+    public void LinkBuilder(float searchRadius)
+    {
+        // Find the instances of vertices in the scene
+        GameObject[] vertices = GameObject.FindGameObjectsWithTag(vertexPrefab.tag);
+
+        navMeshVertices.AddRange(vertices);
+
+        // Iterate through the vertices
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            // Iterate through the vertices again
+            for (int j = 0; j < vertices.Length; j++)
+            {
+                // If the first vertex and the second are not the same
+                if (vertices[i] != vertices[j])
+                {
+                    // If the vertices are within a radius
+                    if (Vector3.Distance(vertices[i].transform.position, vertices[j].transform.position) <= searchRadius)
+                    {
+                        // If the midpoint of the vertices does not have a NavMesh hit
+                        if (!NavMesh.SamplePosition((vertices[i].transform.position + vertices[j].transform.position) / 2, out NavMeshHit hit, 1, NavMesh.AllAreas))
+                        {
+                            // create an OffMeshLink object between the vertices
+                            OffMeshLink offMeshLink = vertices[i].AddComponent<OffMeshLink>();
+
+                            // Set the start position of the offMeshLink to the vertexPrefab with the larger Y value
+                            if (vertices[i].transform.position.y > vertices[j].transform.position.y)
+                            {
+                                offMeshLink.startTransform = vertices[i].transform;
+                                offMeshLink.endTransform = vertices[j].transform;
+                            }
+                            else
+                            {
+                                offMeshLink.startTransform = vertices[j].transform;
+                                offMeshLink.endTransform = vertices[i].transform;
+                            }
+
+                            // set the OffMeshLink biDirectional to true
+                            offMeshLink.biDirectional = true;
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    // A function to draw the triangles of the NavMesh
+    public void NavMesh2Triangles()
+    {
         // Get the triangles from the NavMesh
         NavMeshTriangulation navMeshTriangles = NavMesh.CalculateTriangulation();
 
         // Count the triangles on the NavMesh
         int navMeshTriangleCount = navMeshTriangles.indices.Length / 3;
         Debug.Log("NavMesh triangles counted: " + navMeshTriangleCount);
-        
+
 
         // Go through each triangle on the NavMesh
         for (int i = 0; i < navMeshTriangles.indices.Length; i += 3)
@@ -65,29 +127,20 @@ public class NavMeshManagerBehavior : MonoBehaviour
             // Go through each nearestEdgeHit
             for (int j = 0; j < nearestEdgeHits.Length; j++)
             {
-                                
+
                 // Check if the distance between the NavMeshTriangle vertices to the nearestEdgeHit is less than 0.1f
                 if (Vector3.Distance(navMeshTriangles.vertices[navMeshTriangles.indices[i + j]], nearestEdgeHits[j].position) < 0.1f)
                 {
-                    // Add the corner of the triangle to the list of navMeshEdgeCorners
+                    // Add the vertex of the triangle to the list of navMeshEdgeCorners
                     navMeshEdgeCorners.Add(navMeshTriangles.vertices[navMeshTriangles.indices[i + j]]);
 
                     // Spawn a vertexPrefab at the corner of the triangle on the NavMesh edge as a child of this object
                     GameObject vertex = Instantiate(vertexPrefab, navMeshTriangles.vertices[navMeshTriangles.indices[i + j]], Quaternion.identity, this.transform);
+
                 }
-                
 
             }
-
-
         }
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
 }
